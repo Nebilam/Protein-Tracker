5ee8a9d1432f2862c0452e02354aae9d74f4c902
Nebilam <49345234+Nebilam@users.noreply.github.com>
2024-08-28 13:29:26 +0200

    feat: add ui profile page

--- lib/presentation/popups/profile/profile.dart
+++ lib/presentation/popups/profile/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:myapp/common/widgets/buttons/text_button.dart';
+import 'package:myapp/common/widgets/input_fields/input_field.dart';
 import 'package:myapp/core/riverpod/riverpod.dart';
 
 class Profile extends ConsumerWidget {
@@ -12,17 +13,96 @@ class Profile extends ConsumerWidget {
       appBar: AppBar(
         title: const Text("Profile Settings"),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(120.0),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 100,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 250,
+            ),
+            const CircleAvatar(
+              radius: 100,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.image),
+              label: const Text('Change Profile Picture'),
+            ),
+            const SizedBox(
+              height: 00,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  // const Text("Weight:"),
+                  // const SizedBox(width: 12),
+                  Expanded(
+                    child: InputField(
+                      hint: "Weight",
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  // const Text("Protein ratio:"),
+                  // const SizedBox(width: 12),
+                  Expanded(
+                    child: InputField(
+                      hint: "Protein ratio",
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 20),
+            //   child: InputField(hint: "Weight"),
+            // ),
+            // const SizedBox(
+            //   height: 20,
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 20),
+            //   child: InputField(hint: "Protein ratio"),
+            // ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: InputField(
+                hint: "Username",
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ButtonText(
+              text: "Update",
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // const Spacer(),
+            ButtonText(
+              text: "Change password",
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ButtonText(
+              text: "Sign out",
+              onPressed: () {},
+              style: Style.secondary,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
         ),
       ),
     );
